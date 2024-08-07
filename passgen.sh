920a9aee93caeee2887d0777dcde722b28d0af6f
h1tiz <rfsgoatt@gmail.com>
2024-08-07 19:26:52 +0000

    Update 1.3.0

--- passgen.sh
+++ passgen.sh
@@ -1,54 +1,88 @@
-#!/bin/bash
+ #!/bin/bash
 
+YELLOW='\033[0;033m'
 RED='\033[0;31m'
 NC='\033[0m'
 LENGHT=8
 NUM_WORDS=8
 
+
+cat << '$EOF'
+ _______  _______  _______  _______  _______  _______  __    _ 
+|       ||   _   ||       ||       ||       ||       ||  |  | |
+|    _  ||  |_|  ||  _____||  _____||    ___||    ___||   |_| |
+|   |_| ||       || |_____ | |_____ |   | __ |   |___ |       |
+|    ___||       ||_____  ||_____  ||   ||  ||    ___||  _    |
+|   |    |   _   | _____| | _____| ||   |_| ||   |___ | | |   |
+|___|    |__| |__||_______||_______||_______||_______||_|  |__|
+                                            
+                                           Made by H1tiz
+$EOF
+
+printf  "\n\nOPTIONS\n\n[1] Password Generator\n[2] Support\n[3] Version\n"
+read -r firstopt
+
+passwords(){
+
 RANDOM_WORD=$(cat /dev/urandom | tr -dc 'a-zA-Z' |
  fold -w $LENGHT | head -n 1)}
 
-echo "=======PASS GEN 1.0======="
 
-echo "Options: "
+printf "\nOptions: "
 
-echo -e "PASSWORD FOR XBOX\nCommand: -x\nPASSWORD FOR GOOGLE\nCommand: -g\nPASSWORD FOR PLAYSTATION\nCommand: -p\nPASSWORD FOR MICROSOFT\nCommand: -m\nPASSWORD FOR ROBLOX\nCommand: -r\nPASSWORD FOR GMAIL\nCommand: -gm"
+printf "\n\nPASSWORD FOR XBOX\nCommand: -x\nPASSWORD FOR GOOGLE\nCommand: -g\nPASSWORD FOR PLAYSTATION\nCommand: -p\nPASSWORD FOR MICROSOFT\nCommand: -m\nPASSWORD FOR ROBLOX\nCommand: -r\nPASSWORD FOR GMAIL\nCommand: -gm\n"
 read option
 
 if [ "$option" = "-x" ]; then
 
-  echo "Generated password for XBOX: $RANDOM_WORD"
- echo "XBOX PASS: $RANDOM_WORD" >> Passwords.txt
-  echo "Password saved in Passwords.txt"
+  printf "\nGenerated password for XBOX: $RANDOM_WORD"
+ printf "\nXBOX PASS: $RANDOM_WORD" >> Passwords.txt
+  printf  "\nPassword saved in Passwords.txt"
 
 elif [ "$option" = "-g" ]; then
 
-  echo "Generated password for GOOGLE: $RANDOM_WORD"
-  echo "GOOGLE PASS: $RANDOM_WORD" >> Passwords.txt
-  echo "Password saved in Passwords.txt"
+  printf "\nGenerated password for GOOGLE: $RANDOM_WORD"
+  printf "\nGOOGLE PASS: $RANDOM_WORD" >> Passwords.txt
+  printf "\nPassword saved in Passwords.txt"
 
 elif [ "$option" = "-p" ]; then
- echo "Generated password for PLAYSTATION: $RANDOM_WORD"
- echo "PLAYSTATIONS PASS: $RANDOM_WORD" >> Passwords.txt
- echo "Password saved in Passwords.txt"
+ printf "\nGenerated password for PLAYSTATION: $RANDOM_WORD"
+ printf "\nPLAYSTATIONS PASS: $RANDOM_WORD" >> Passwords.txt
+ printf "\nPassword saved in Passwords.txt"
 
 elif [ "$option" = "-m" ]; then
-  echo "Generated password for Microsoft: $RANDOM_WORD"
-  echo "MICROSOFT PASS: $RANDOM_WORD" >> Passwords.txt
-  echo "Password saved in Passwords.txt"
+  printf "\nGenerated password for Microsoft: $RANDOM_WORD"
+  printf "\nMICROSOFT PASS: $RANDOM_WORD" >> Passwords.txt
+  printf "\nPassword saved in Passwords.txt"
 
 elif [ "$option" = "-r" ]; then
-  echo "Generated password for Roblox: $RANDOM_WORD"
-  echo "ROBLOX PASS: $RANDOM_WORD" >> Passwords.txt
- echo "Password saved in Passwords.txt"
+  printf "\nGenerated password for Roblox: $RANDOM_WORD"
+  printf "\nROBLOX PASS: $RANDOM_WORD" >> Passwords.txt
+ printf "\nPassword saved in Passwords.txt"
 
 elif [ "$option" = "-gm" ]; then
-  echo "Generated password for Gmail: $RANDOM_WORD"
-  echo "GMAIL PASS: $RANDOM_WORD" >> Passwords.txt
-  echo "Password saved in passwords.txt"
+  printf "\nGenerated password for Gmail: $RANDOM_WORD"
+  printf "\nGMAIL PASS: $RANDOM_WORD" >> Passwords.txt
+  printf "\nPassword saved in passwords.txt"
 
 
 else
-  echo "Invalid option. Please try again."
+  echo "$Non existant option. Please try again."
+fi
+}
+
+if [ "$firstopt" = "1" ] || [ "$firstopt" = "01" ]; then
+passwords
+
+elif [ "$firstopt" = "2" ] || [ "$firstopt" = "02" ]; then
+
+printf "If you have any issue or questions about the software, contact us at Telegram:\nhttps://t.me/+3W9MyCD7MSE1M2Ex"
+
+elif  [ "$firstopt" = "3" ] || [ "$firstopt" =  "03" ]; then
+
+printf "PassGen V1.3.0\nMade by  H1tiz (c)"
+
+else
+ echo "Invalid option. Please try again." 
 
 fi
